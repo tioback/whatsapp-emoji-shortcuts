0a629e9f2a8b888bc9de5c1ec57bb05c18a38074
Renato Pereira Back <renatopb@gmail.com>
2018-09-21 15:52:28 -0300

    Added pt-BR files.

--- emoji-reader.js
+++ emoji-reader.js
@@ -1,14 +1,19 @@
 "use strict";
 exports.__esModule = true;
 var emojis_en_US_1 = require("./emojis_en-US");
+var emojis_pt_BR_1 = require("./emojis_pt-BR");
 var shortcutMap = {};
-Object.keys(emojis_en_US_1.emojis).forEach(function (shortcut) {
-    var value = emojis_en_US_1.emojis[shortcut];
-    value.forEach(function (emoji) {
-        if (!shortcutMap[emoji]) {
-            shortcutMap[emoji] = [];
-        }
-        shortcutMap[emoji].push(shortcut);
+var mapUpdater = function (map) {
+    Object.keys(map).forEach(function (shortcut) {
+        var value = map[shortcut];
+        value.forEach(function (emoji) {
+            if (!shortcutMap[emoji]) {
+                shortcutMap[emoji] = [];
+            }
+            shortcutMap[emoji].push(shortcut);
+        });
     });
-});
+};
+mapUpdater(emojis_en_US_1.emojis);
+mapUpdater(emojis_pt_BR_1.emojis);
 Object.keys(shortcutMap).forEach(function (emoji, index) { return console.log(index + 1, "Emoji", emoji, "has shortcuts", shortcutMap[emoji]); });
--- emoji-reader.ts
+++ emoji-reader.ts
@@ -1,15 +1,21 @@
-import { emojis } from "./emojis_en-US";
+import { emojis as emojisEN } from "./emojis_en-US";
+import { emojis as emojisPT } from "./emojis_pt-BR";
 
 const shortcutMap: any = {};
 
-Object.keys(emojis).forEach(shortcut => {
-    const value: string[] = emojis[shortcut];
-    value.forEach(emoji => {
-        if (!shortcutMap[emoji]) {
-            shortcutMap[emoji] = [];
-        }
-        (shortcutMap[emoji] as string[]).push(shortcut);
-    });
-});
+const mapUpdater = (map) => {
+	Object.keys(map).forEach(shortcut => {
+	    const value: string[] = map[shortcut];
+	    value.forEach(emoji => {
+	        if (!shortcutMap[emoji]) {
+	            shortcutMap[emoji] = [];
+	        }
+	        (shortcutMap[emoji] as string[]).push(shortcut);
+	    });
+	});
+}
+
+mapUpdater(emojisEN);
+mapUpdater(emojisPT);
 
 Object.keys(shortcutMap).forEach((emoji, index) => console.log(index + 1, "Emoji", emoji, "has shortcuts", shortcutMap[emoji]));
